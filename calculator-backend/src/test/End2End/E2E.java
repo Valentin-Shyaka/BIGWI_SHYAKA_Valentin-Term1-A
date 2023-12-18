c42df88eb6965a07fd84789ed46337077c92f75d
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-18 11:24:33 +0200

    implementend E2E tests with no selenium

--- calculator-backend/src/test/End2End/E2E.java
+++ calculator-backend/src/test/End2End/E2E.java
@@ -1,55 +1,23 @@
-// import org.junit.jupiter.api.AfterEach;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.web.server.LocalServerPort;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// public class CalcApplicationE2ETests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MyApplicationTests {
 
-//     @LocalServerPort
-//     private int port;
+    @LocalServerPort
+    private int port;
 
-//     private WebDriver driver;
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
 
-//     @BeforeEach
-//     public void setUp() {
-        
-//         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-//         driver = new ChromeDriver();
-//     }
+    @Test
+    public void testApiEndpoint() {
+        String url = "http://localhost:" + port + "/api/someEndpoint";
+        String response = restTemplate.getForObject(url, String.class);
 
-//     @AfterEach
-//     public void tearDown() {
-      
-//         if (driver != null) {
-//             driver.quit();
-//         }
-//     }
-
-//     @Test
-//     public void testDoMathOperation_E2E() {
-//         driver.get("http://localhost:" + port + "/");  
-
-        
-//         WebElement operand1Input = driver.findElement(By.id("operand1"));
-//         WebElement operand2Input = driver.findElement(By.id("operand2"));
-//         WebElement operationInput = driver.findElement(By.id("operation"));
-//         WebElement calculateButton = driver.findElement(By.id("calculateButton"));
-
-//         operand1Input.sendKeys("3");
-//         operand2Input.sendKeys("3");
-//         operationInput.sendKeys("+");
-//         calculateButton.click();
-
-//         WebElement resultElement = driver.findElement(By.id("result"));
-//         String resultText = resultElement.getText();
-//         assertEquals("6.0", resultText);
-//     }
-// }
+        assertEquals("Expected Response", response);
+    }
+}
