828b0ff5ceda949e6a6eda4acd139e9dcc8a72fb
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-18 09:18:13 +0200

    implemented the API response...

---
+++ calculator-backend/src/main/java/com/example/calculator/utils/APIResponse.java
@@ -0,0 +1,10 @@
+package com.example.calculator.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class APIResponse {
+    private double calcResponse;
+}
