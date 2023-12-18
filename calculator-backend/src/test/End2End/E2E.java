380d43d2c77ab6399913d5fd606b70440361e616
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-18 11:25:49 +0200

    [Fix]: E2E tests implementation issues fixed...

--- calculator-backend/src/test/End2End/E2E.java
+++ calculator-backend/src/test/End2End/E2E.java
@@ -1,22 +1,36 @@
-import org.junit.Test;
+package rca.exam.calculator.endtoend;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class MyApplicationTests {
+public class MathApplicationTestE2E {
 
     @LocalServerPort
     private int port;
 
-    private final TestRestTemplate restTemplate = new TestRestTemplate();
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void testApiEndpoint() {
-        String url = "http://localhost:" + port + "/api/someEndpoint";
-        String response = restTemplate.getForObject(url, String.class);
+
+        String url = "http://localhost:" + port + "/api/v1/do_math";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestBody = "{\"operand1\": 2, \"operand2\": 5, \"operation\": \"+\"}";
+
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        String response = restTemplate.postForObject(url, request, String.class);
 
         assertEquals("Expected Response", response);
     }
