758569b8baee4f2484d5b43e611d6eb300d37815
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-18 09:19:41 +0200

    implemented class to deal with invalid operations

---
+++ calculator-backend/src/main/java/com/example/calculator/utils/InvalidOperationException.java
@@ -0,0 +1,8 @@
+package com.example.calculator.utils;
+
+public class InvalidOperationException extends Exception {
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
+
