b07ebc74192986125df58005df71db61a58df558
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-12-18 10:02:40 +0200

    added calculator application E2E tests...

---
+++ calculator-backend/src/test/CalcApplicationE2ETests.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CalcApplicationE2ETests {
+
+    @LocalServerPort
+    private int port;
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up WebDriver (assuming you have ChromeDriver installed)
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Close the browser after each test
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testDoMathOperation_E2E() {
+        // Given
+        driver.get("http://localhost:" + port + "/");  // Assuming your application is running at the root context
+
+        // Find input fields and submit button
+        WebElement operand1Input = driver.findElement(By.id("operand1"));
+        WebElement operand2Input = driver.findElement(By.id("operand2"));
+        WebElement operationInput = driver.findElement(By.id("operation"));
+        WebElement calculateButton = driver.findElement(By.id("calculateButton"));
+
+        // When
+        operand1Input.sendKeys("3");
+        operand2Input.sendKeys("3");
+        operationInput.sendKeys("+");
+        calculateButton.click();
+
+        // Then
+        WebElement resultElement = driver.findElement(By.id("result"));
+        String resultText = resultElement.getText();
+        assertEquals("6.0", resultText);
+    }
+}
